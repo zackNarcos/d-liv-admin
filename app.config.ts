13b7697dca5f888a261e9e11be549dc1fc862bdf
zackNarcos <zackabess@gmail.com>
2025-05-04 11:50:25 +0000

    Update API configuration with new production endpoints

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
 export default defineAppConfig({
-  // api: {
-  //   base: "https://vmi2269336.contaboserver.net",
-  //   auth: "https://auth.dliv.zack-abessolo.com",
-  // },
   api: {
-    base: "http://localhost:3003",
-    auth: "http://localhost:3001",
+    base: "https://vmi2584756.contaboserver.net",
+    auth: "https://vmi2584756.contaboserver.net",
   },
+  // api: {
+  //   base: "http://localhost:3003",
+  //   auth: "http://localhost:3001",
+  // },
 });
