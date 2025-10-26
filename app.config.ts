bbbf669a594fd880d4150c9c9ce545d28a703d8a
zackNarcos <zackabess@gmail.com>
2025-10-26 19:27:07 +0000

    Update API configuration with new production URLs

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
 export default defineAppConfig({
-  api: { base: "https://d-livz.com/api", auth: "https://d-livz/api", },
+  api: { base: "https://www.d-livz.com/api", auth: "https://www.d-livz/api", },
   // api: { base: "http://localhost:3003/api", auth: "http://localhost:3001/api", },
 });
